0451709a6a7174912d324f549a630a09122e92c2
gaoqi <380944602@qq.com>
2022-07-27 19:51:15 +0800

    modify

--- scripts/gpxtrackposter/track.py
+++ scripts/gpxtrackposter/track.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 
-import datetime
+import datetime 
 import os
 from collections import namedtuple
 
